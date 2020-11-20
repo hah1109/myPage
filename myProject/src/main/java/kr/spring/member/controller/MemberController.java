1c5ec32ce94f2c581382d808e15ff8accfe9d2b3
Taylor <Taylor@DESKTOP-2U9AV6M>
2020-11-20 19:07:00 +0900

    xml 파일 꼭 다운받고 작업 시작 할 것.

--- myProject/src/main/java/kr/spring/member/controller/MemberController.java
+++ myProject/src/main/java/kr/spring/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,144 +19,189 @@ import kr.spring.util.LoginCheckException;
 @Controller
 public class MemberController {
 	
+	//로그 처리
+	private Logger log = Logger.getLogger(this.getClass());
+	
 	@Resource
 	private MemberService memberService;
 	
-	
+	/*========================================================================================================*/
+	//로그인 폼에서 회원가입 버튼을 눌렀을시 호출될 메서드
 	//유저 타입 폼
 	//처리할게 없으니 get/post 안씀
 	@RequestMapping("/member/userType.do")
 	public String formUserType() {
-
-		/*
-		if(일반회원){
-			//definition 설정명을 호출
-			return “memberRegister”;
-		}
-		else{//트레이너
-			//definition 설정명을 호출
-			return “trainerRegister”
-			
-		}
-		*/
-		
-		return "";
+		//definition 설정명을 호출
+		return "userType";//이 뷰에서 일반회원/트레이너 2개의 버튼으로 회원가입 폼을 호출할 예정
 	}
 
+	/*========================================================================================================*/
+	
+	/*========================================================================================================*/
+	//유저타입 폼에서 일반회원 버튼 클릭시에 호출할 메서드
 	//일반회원 회원가입 폼
 	@RequestMapping(value="/member/memberRegister.do", method=RequestMethod.GET)
 	public String formMemberRegister() {
 		//definition 설정명을 호출
-		return "memberRegister";
+		return "memberRegister";//이 뷰에서 일반회원의 회원정보를 기입할 수 있게 해준다
 	}
 
+	/*========================================================================================================*/
+	
+	/*========================================================================================================*/
+	//일반회원 회원정보를 기입하고 회원가입 버튼을 눌렀을 경우 호출될 메서드
 	//일반회원 회원가입 처리
 	@RequestMapping(value="/member/memberRegister.do", method=RequestMethod.POST)
 	public String submitMemberRegister(@Valid MemberVO memberVO, BindingResult result) {
 
+		//로그 정보로 데이터가 넘어오는 과정을 보여줌
+		if(log.isDebugEnabled()) {
+			log.debug("<<<일반회원 회원정보>>> : " + memberVO);
+		}
+				
+		//유효성 체크 결과 오류가 있으면 폼 호출
+		if(result.hasErrors()) {
+			return formMemberRegister();
+		}
 		
+		//유효성 체크 결과 정상일 경우 쿼리문 작업
 		memberService.insertMember_detail(memberVO);
 		
-		
-		//definition 설정명을 호출
-		return "redirect:/main/main.do";
+		//일반회원 회원정보를 테이블에 정상적으로 등록했을 경우 로그인 페이지로 이동
+		return "redirect:/member/login.do";
 	}
 
+	/*========================================================================================================*/
+	
+	/*========================================================================================================*/
+	//유저타입 폼에서 트레이너 버튼 클릭시에 호출할 메서드
 	//트레이너 회원가입 폼
 	@RequestMapping(value="/member/trainerRegister.do", method=RequestMethod.GET)
-	public String form() {
+	public String formTrainerRegister() {
 		//definition 설정명을 호출
 		return "trainerRegister";
 	}
-
+	
+	/*========================================================================================================*/
+	
+	/*========================================================================================================*/
+	//트레이너 회원정보를 기입하고 회원가입 버튼을 눌렀을 경우 호출될 메서드
 	//트레이너 회원가입 처리
 	@RequestMapping(value="/member/trainerRegister.do", method=RequestMethod.POST)
 	public String submitTrainerRegister(@Valid MemberVO memberVO, BindingResult result) {
 		
+		//로그 정보로 데이터가 넘어오는 과정을 보여줌
+		if(log.isDebugEnabled()) {
+			log.debug("<<<트레이너 회원정보>>> : " + memberVO);
+		}
+						
+		//유효성 체크 결과 오류가 있으면 폼 호출
+		if(result.hasErrors()) {
+			return formTrainerRegister();
+		}
 		
+		//유효성 체크 결과 정상일 경우 쿼리문 작업
 		memberService.insertTrainer_detail(memberVO);
 		
-		//definition 설정명을 호출
-		return "redirect:/main/main.do";
+		//트레이너 회원정보를 테이블에 정상적으로 등록했을 경우 로그인 페이지로 이동
+		return "redirect:/member/login.do";
 	}
-
+	
+	/*========================================================================================================*/
+	
+	/*========================================================================================================*/
+	//로그인 폼을 호출하는 메서드 [회원가입/로그인/(id/pw)찾기] 버튼으로 3가지 동작이 있음
 	//로그인 폼
 	@RequestMapping(value="/member/login.do", method=RequestMethod.GET)
 	public String formLogin() {	
-		return "memberLogin";
+		//tiles-def 폴더에 member.xml파일의 definition name 호출
+		return "login";
 	}
-
-	//일반회원 로그인 처리
-	@RequestMapping(value="/member/memberlogin.do", method=RequestMethod.POST)
-	public String submitMemberLogin(@Valid MemberVO memberVO, BindingResult result, HttpSession session) {
-
-		//아이디와 비밀번호가 일치하는지 확인
-		try {
-
-			MemberVO member = memberService.selectCheckMember_detail(memberVO.getMem_id());
-			boolean check = false;
-
-			//id값이 있는 경우
-			if(memberVO != null) {
-				//비밀번호 일치여부 체크
-				check = memberVO.isCheckedPasswd(member.getMem_pw());
-			}
-
-			if(check) {
-				//인증 성공, 로그인 처리
-				session.setAttribute("user", member);
-				return "redirect:/main/main.do";
-			}else {
-				//인증 실패
-				throw new LoginCheckException();
-			}
-
-		}catch(LoginCheckException e) {
-			//인증 실패로 로그인폼 호출
-			result.reject("invalidIdOrPassword");
-
+	
+	/*========================================================================================================*/
+	
+	/*========================================================================================================*/
+	//로그인 폼에서 콤보박스로 일반회원/트레이너를 선택하고 로그인 버튼을 눌렀을 시 호출
+	//일반회원/트레이너 로그인 처리
+	@RequestMapping(value="/member/login.do", method=RequestMethod.POST)
+	public String submitLogin(@Valid MemberVO memberVO, BindingResult result, HttpSession session) {
+								//memberVO에는 로그인 폼에서 유저가 입력한 데이터가 들어있음
+		//로그 정보로 데이터가 넘어오는 과정을 보여줌
+		if(log.isDebugEnabled()) {
+			log.debug("<<<유저의 회원정보>>> : " + memberVO);
+		}
+				
+		//유효성 체크(id,passwd) 결과 오류가 있으면 폼 호출
+		if(result.hasFieldErrors("id") || result.hasFieldErrors("passwd") || result.hasFieldErrors("auth")) {
 			return formLogin();
 		}
-	}
-
-
-
-
-	//트레이너 로그인 처리
-	@RequestMapping(value="/member/trainerlogin.do", method=RequestMethod.POST)
-	public String submitTrainerLogin(@Valid MemberVO memberVO, BindingResult result, HttpSession session) {
-
-	//아이디와 비밀번호가 일치하는지 확인
-	try {
-		MemberVO trainer = memberService.selectCheckTrainer_detail(memberVO.getMem_id());
-		boolean check = false;
-				
-		//id값이 있는 경우
-		if(memberVO != null) {
-			//비밀번호 일치여부 체크
-			check = memberVO.isCheckedPasswd(trainer.getMem_pw());
-		}
-				
-		if(check) {
-			//인증 성공, 로그인 처리
-			session.setAttribute("user", trainer);
-			return "redirect:/main/main.do";
-		}else {
-			//인증 실패
-			throw new LoginCheckException();
-			}
-				
+		
+		//일반회원 일경우 로그인 처리
+		if(memberVO.getMem_auth() == 1) {
+			//아이디와 비밀번호가 일치하는지 확인
+			try {
+				//member에는 사용자가 입력한 아이디를 테이블에서 검색후 해당 레코드를 가져온 정보가 들어있음
+				MemberVO member = memberService.selectCheckMember_detail(memberVO.getMem_id());
+				boolean check = false;
+	
+				//id값이 있는 경우
+				if(member != null) {
+					//비밀번호 일치여부 체크
+					check = member.isCheckedPasswd(memberVO.getMem_pw());
+				}
+	
+				if(check) {
+					//인증 성공, 로그인 처리
+					session.setAttribute("user", member);
+					return "redirect:/main/main.do";
+				}else {
+					//인증 실패
+					throw new LoginCheckException();
+				}
+	
 			}catch(LoginCheckException e) {
 				//인증 실패로 로그인폼 호출
 				result.reject("invalidIdOrPassword");
-				
+				//definition 설정명을 호출
 				return formLogin();
 			}
+		}else if(memberVO.getMem_auth() == 2) {//트레이너일경우 로그인처리
+			//아이디와 비밀번호가 일치하는지 확인
+			try {
+				//member에는 사용자가 입력한 아이디를 테이블에서 검색후 해당 레코드를 가져온 정보가 들어있음
+				MemberVO trainer = memberService.selectCheckTrainer_detail(memberVO.getMem_id());
+				boolean check = false;
+	
+				//id값이 있는 경우
+				if(trainer != null) {
+					//비밀번호 일치여부 체크
+					check = trainer.isCheckedPasswd(memberVO.getMem_pw());
+				}
+	
+				if(check) {
+					//인증 성공, 로그인 처리
+					session.setAttribute("user", trainer);
+					return "redirect:/main/main.do";
+				}else {
+					//인증 실패
+					throw new LoginCheckException();
+				}
+	
+			}catch(LoginCheckException e) {
+				//인증 실패로 로그인폼 호출
+				result.reject("invalidIdOrPassword");
+				//definition 설정명을 호출
+				return formLogin();
+			}
+		}
+		
+		//definition 설정명을 호출
+		return formLogin();
 	}
-			
 
-
-
+	/*========================================================================================================*/
+	
+	/*========================================================================================================*/
 	//로그아웃 처리
 	@RequestMapping("/member/logout.do")
 	public String processLogout(HttpSession session) {	
@@ -167,23 +213,14 @@ public class MemberController {
 		return "redirect:/main/main.do";
 		}
 
-
-
+	/*===여기 까지 함============================================================================================*/
+	
+	//로그인 폼에서 id/pw버튼을 클릭했을시 호출될 메서드
 	//id/pw 찾기 폼
 	@RequestMapping(value="/member/findIdPw.do", method=RequestMethod.GET)
 	public String formFindIdPw() {
-
-		/*
-		if(일반회원){
-			//definition 설정명을 호출
-			return “findMemberIdPw”;
-		}
-		else{//트레이너
-			//definition 설정명을 호출
-			return “findTrainerIdPw”
-		}
-		*/
-		return "";
+		//definition 설정명 호출
+		return "findIdPw";
 	}
 
 	//일반회원 id/pw 찾기 처리
@@ -213,37 +250,28 @@ public class MemberController {
 
 	//일반회원 상세 정보
 	//마이페이지에 보여질 정보
-	@RequestMapping("/member/myPageMember.do")
-	public String myPageMember(HttpSession session, Model model) {
+	@RequestMapping("/member/myPage.do")
+	public String myPage(HttpSession session, Model model) {
 			
 		//회원번호를 얻기위해 세션에 저장된 회원 정보를 반환
 		MemberVO memberVO = (MemberVO)session.getAttribute("user");
-		
-		//회원번호를 넣어준 다음 정보를 읽어옴
-		MemberVO member = memberService.selectMember_detail(memberVO.getMem_num());
-		
-		//읽어온 정보를 모델을 정해서 request에 저장
-		model.addAttribute("member", member);
+		if(memberVO.getMem_auth() == 1) {
+			//회원번호를 넣어준 다음 정보를 읽어옴
+			MemberVO member = memberService.selectMember_detail(memberVO.getMem_num());
 			
-		return "memberView";
-	} 
-
-	//트레이너 상세 정보
-	//마이페이지에 보여질 정보
-	@RequestMapping("/member/myPageMember.do")
-	public String myPageTrainer(HttpSession session, Model model) {
-			
-		//회원번호를 얻기위해 세션에 저장된 회원 정보를 반환
-		MemberVO memberVO = (MemberVO)session.getAttribute("user");
+			//읽어온 정보를 모델을 정해서 request에 저장
+			model.addAttribute("member", member);
+			return "memberView";
+		}else if(memberVO.getMem_auth() == 2) {
+			//회원번호를 넣어준 다음 정보를 읽어옴
+			MemberVO trainer = memberService.selectTrainer_detail(memberVO.getMem_num());
+			//읽어온 정보를 모델을 정해서 request에 저장
+			model.addAttribute("trainer", trainer);
+			return "trainerView";
+		}
+		return "redirect:/member/login.do";
 		
-		//회원번호를 넣어준 다음 정보를 읽어옴
-		MemberVO trainer = memberService.selectTrainer_detail(memberVO.getMem_num());
-		
-		//읽어온 정보를 모델을 정해서 request에 저장
-		model.addAttribute("trainer", trainer);
-			
-		return "trinerView";
-	} 
+	}  
 
 	//일반회원 정보 수정 폼
 	@RequestMapping(value="/member/updateMember.do",method=RequestMethod.GET)
@@ -277,7 +305,7 @@ public class MemberController {
 	}
 
 	//회원 정보 수정 처리
-	@RequestMapping(value="/member/update.do",method=RequestMethod.POST)
+	@RequestMapping(value="/member/memberUpdate.do",method=RequestMethod.POST)
 	public String submitUpdateMember(@Valid MemberVO memberVO, BindingResult result, HttpSession session) {
 			
 			
@@ -294,7 +322,7 @@ public class MemberController {
 	}
 
 	//회원 정보 수정 처리
-	@RequestMapping(value="/member/update.do",method=RequestMethod.POST)
+	@RequestMapping(value="/member/TrainerUpdate.do",method=RequestMethod.POST)
 	public String submitUpdateTrainer(@Valid MemberVO memberVO, BindingResult result, HttpSession session) {
 			
 			
