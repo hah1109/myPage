12f60719c7002e1105574f4157f88101c6e9ccc3
Taylor <Taylor@DESKTOP-2U9AV6M>
2020-11-30 14:23:33 +0900

    오류 복구

--- myProject/src/main/java/kr/spring/board/feed/controller/FeedController2.java
+++
@@ -1,9 +0,0 @@
-package kr.spring.board.feed.controller;
-
-import org.springframework.stereotype.Controller;
-
-//홍아현
-@Controller
-public class FeedController2 {
- 
-}
--- myProject/src/main/java/kr/spring/board/feed/controller/FeedController3.java
+++
@@ -1,9 +0,0 @@
-package kr.spring.board.feed.controller;
-
-import org.springframework.stereotype.Controller;
-
-//조재희
-@Controller
-public class FeedController3 {
- 
-}
